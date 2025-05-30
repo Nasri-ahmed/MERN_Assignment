5e3f8c6b5e0c2c83f4bd0d767656e5f9d3169b8f
Nasri Ahmed <info9345@gmail.com>
2025-05-30 17:21:23 +0100

    Convert each function below into ES6/Arrow function syntax!

---
+++ ReactJS/React Props/Prop/src/components/PersonCard.jsx
@@ -0,0 +1,15 @@
+import '../App.css'; // pour les styles
+
+const PersonCard = (props) => {
+    const { firstName, lastName, age, hairColor } = props;
+
+    return (
+        <div className="personcard">
+            <h2>{firstName}, {lastName}</h2>
+            <p>Age:{age}</p>
+            <p>Hair Color: {hairColor}</p>
+        </div>
+    );
+}
+
+export default PersonCard;
